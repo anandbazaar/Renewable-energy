1d74b8e63a6ad29e2488168e672af89891ec7aea
Maralmaa <maralmaa@Zorigs-MacBook-Pro.local>
2022-04-09 14:49:29 +0800

    quiz

---
+++ quiz/quiz.js
@@ -0,0 +1,27 @@
+const quiz =  [
+    {
+        q:'Whats Your Favorite TV Show?',
+        options:['1','2'],
+        answer:0
+    },
+    {
+        q:'Fav Color?',
+        options:['1','2','3','4'],
+        answer:2
+    },
+    {
+        q:'Whats Your Name?',
+        options:['true','false'],
+        answer:1
+    },
+    {
+        q:'Whats Your Favorite TV Show?',
+        options:['1','2','3'],
+        answer:0
+    },
+    {
+        q:'Whats Your Favorite TV Show?',
+        options:['1','2','3','4'],
+        answer:3
+    }
+]
