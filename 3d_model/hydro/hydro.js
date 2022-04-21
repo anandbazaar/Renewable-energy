1c4e347544d9600354fa1d0ffd60c13d1657e7d7
anandbazar <Sigmamale@LEAP-17s-iMac.local>
2022-04-21 19:00:47 +0800

    3d model

--- 3d_model/hydro/hydro.js
+++ 3d_model/hydro/hydro.js
@@ -1,7 +1,8 @@
 const scene = new THREE.Scene();
+scene.background = new THREE.Color(0x152744);
 
 // Panel
-
+const hydro = new THREE.Group();
 const walls = new THREE.Group();
 const waals =  new THREE.Group();
 
@@ -16,7 +17,7 @@ const wall3 = new THREE.Mesh(geometry, material);
 const wall4 = new THREE.Mesh(geometry, material);
 
 // Side Walls
-geometry = new THREE.BoxGeometry(20, 600, 300);
+geometry = new THREE.BoxGeometry(20, 300, 300);
 material = new THREE.MeshBasicMaterial({ map: texture });
 
 const wall5 = new THREE.Mesh(geometry, material);
@@ -37,7 +38,7 @@ const params = {
     flowY: 1,
   };
   
-  let waterGeometry = new THREE.CylinderGeometry(250, 250, 480,64);
+  let waterGeometry = new THREE.CylinderGeometry(250, 250, 480,64,1,false,0,Math.PI);
 
   water = new Water(waterGeometry, {
     color: params.color,
@@ -54,7 +55,7 @@ waterGeometry = new THREE.PlaneGeometry(1000,1000)
 water = new Water(waterGeometry, {
     color: params.color,
     scale: params.scale,
-    flowDirection: new THREE.Vector2(params.flowX, params.flowY),
+    flowDirection: new THREE.Vector2(0, 100),
     textureWidth: 1024,
     textureHeight: 1024,
   });
@@ -62,7 +63,7 @@ water = new Water(waterGeometry, {
 
   water.rotation.x = Math.PI * 1.5
 
-  water.position.set(50,-190,0);
+  water.position.set(50,-228,0);
   
 
 
@@ -71,7 +72,7 @@ water = new Water(waterGeometry, {
 
 //Cylinder walls
 
-geometry = new THREE.CylinderGeometry(250, 250, 20, 64);
+geometry = new THREE.CylinderGeometry(250, 250, 20, 64,1,false,0,Math.PI);
 material = new THREE.MeshBasicMaterial({ map: texture });
 const waal = new THREE.Mesh(geometry, material);
 const waal2 = new THREE.Mesh(geometry, material);
@@ -105,8 +106,8 @@ wall.position.set(0,45,0);
 wall2.position.set(100,45,0);
 wall3.position.set(200,45,0);
 wall4.position.set(-100,45,0);
-wall5.position.set(-200,-200,-100);
-wall6.position.set(300,-200,-100);
+wall5.position.set(-200,-50,-100);
+wall6.position.set(300,-50,-100);
 waal.position.set(-200,-240,50);
 waal2.position.set(300,-240,50)
 waal3.position.set(100,-240,50);
@@ -116,8 +117,8 @@ waal6.position.set(0,-240,50);
 
 
 
-scene.add(walls);
-scene.add(waals);
+hydro.add(walls);
+hydro.add(waals);
 waals.position.y = -10
 
 //Top
@@ -131,55 +132,35 @@ toop.rotation.z = Math.PI / 2;
 
 toop.position.set(50,110,-100);
 
-geometry = new THREE.BoxGeometry(20, 500, 20);
-material = new THREE.MeshBasicMaterial({ map: texture });
-const toop2 = new THREE.Mesh(geometry, material);
 
-toop2.rotation.z = Math.PI / 2;
+hydro.add(toop);
 
-toop2.position.set(50,-50,-100);
-
-scene.add(toop);
-scene.add(toop2);
-
-//Bottom
-
-geometry = new THREE.BoxGeometry(260, 300, 350);
-material = new THREE.MeshBasicMaterial({ map: texture });
-const Bottam = new THREE.Mesh(geometry, material);
-const Bottam2 = new THREE.Mesh(geometry, material);
-
-Bottam.position.set(180,-350,150);
-Bottam2.position.set(-80,-350,150);
-
-scene.add(Bottam);
-scene.add(Bottam2);
 
 //Between
 texture = new THREE.TextureLoader().load("../textures/water-texture.webp");
-geometry = new THREE.BoxGeometry(20, 460, 510);
+geometry = new THREE.BoxGeometry(20, 230, 510);
 material = new THREE.MeshBasicMaterial({ map: texture });
 const Between = new THREE.Mesh(geometry, material);  
 
 Between.rotation.y = Math.PI / 2;
 
-Between.position.set(50,-275,0)
+Between.position.set(50,-130,0)
 
 
 //-------------------
 
-texture = new THREE.TextureLoader().load("../textures/mossy-stone-texture.jpeg");
-geometry = new THREE.BoxGeometry(20, 600, 510);
+texture = new THREE.TextureLoader().load("../textures/stone-texture.webp");
+geometry = new THREE.BoxGeometry(20, 300, 510);
 material = new THREE.MeshBasicMaterial({ map: texture });
 
 const Between2 = new THREE.Mesh(geometry, material);
 
 Between2.rotation.y = Math.PI / 2;
 
-Between2.position.set(50,-200,-250)
+Between2.position.set(50,-50,-250)
 
-scene.add(Between2);
-scene.add(Between);
+hydro.add(Between2);
+hydro.add(Between);
 
 
 
@@ -191,7 +172,7 @@ const vent =  new THREE.Mesh(geometry, material);
 
 vent.position.set(50,40,0);
  
-scene.add(vent);
+hydro.add(vent);
 
 //Vent holes
 
@@ -229,22 +210,62 @@ hole13.position.set(-50,45,3);
 hole14.position.set(-70,45,3);
 hole15.position.set(-30,45,3);
 
-scene.add(hole);
-scene.add(hole2);
-scene.add(hole3);
-scene.add(hole4);
-scene.add(hole5);
-scene.add(hole6);
-scene.add(hole7);
-scene.add(hole8);
-scene.add(hole9);
-scene.add(hole10);
-scene.add(hole11);
-scene.add(hole12);
-scene.add(hole13);
-scene.add(hole14);
-scene.add(hole15);
+hydro.add(hole);
+hydro.add(hole2);
+hydro.add(hole3);
+hydro.add(hole4);
+hydro.add(hole5);
+hydro.add(hole6);
+hydro.add(hole7);
+hydro.add(hole8);
+hydro.add(hole9);
+hydro.add(hole10);
+hydro.add(hole11);
+hydro.add(hole12);
+hydro.add(hole13);
+hydro.add(hole14);
+hydro.add(hole15);
 
+scene.add(hydro)
+//cloud
+const cloud1 = new THREE.Mesh(
+  new THREE.CylinderGeometry(10, 10, 10, 128),
+  new THREE.MeshLambertMaterial({ color: "white" })
+);
+const squareMesh = new THREE.Mesh(
+  new THREE.BoxGeometry(10, 10, 10),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+const clouds = [];
+squareMesh.rotation.z = 0;
+squareMesh.scale.set(2, 1, 1.1);
+squareMesh.position.set(20, -6, -45);
+squareMesh.updateMatrixWorld();
+cloud1.position.set(20, 0, -45);
+cloud1.rotation.x = Math.PI * 0.5;
+cloud1.updateMatrixWorld();
+const squareCSG = CSG.fromMesh(squareMesh);
+const cloud = CSG.fromMesh(cloud1);
+const cloudCSG = cloud.subtract(squareCSG);
+for (let i = 0; i < 4; i++) {
+  clouds[i] = CSG.toMesh(cloudCSG, new THREE.Matrix4());
+  clouds[i].material = new THREE.MeshBasicMaterial({ color: "white" });
+}
+scene.add(clouds[0]);
+clouds[0].position.set(600, 298, -300);
+clouds[0].scale.set(8, 8, 10);
+scene.add(clouds[1]);
+clouds[1].position.set(600,300,-300)
+clouds[1].scale.set(10, 10, 10);
+scene.add(clouds[2]);
+clouds[2].scale.set(8, 8, 10);
+clouds[2].position.set(-500, 298, -250);
+clouds[2].rotation.y = Math.PI * 0.5;
+
+scene.add(clouds[3]);
+clouds[3].rotation.y = Math.PI * 0.5;
+clouds[3].position.set(-500, 300, -300);
+clouds[3].scale.set(10,10,10)
 // Tree
 
 const tree = new THREE.Group()
@@ -295,51 +316,18 @@ const bushes = []
 texture = new THREE.TextureLoader().load("../textures/bush-texture.jpeg");
 geometry = new THREE.DodecahedronGeometry(100, 1);
 material = new THREE.MeshBasicMaterial({ map: texture });
-for(let i=0;i<15;i++){
+for(let i=0;i<2;i++){
   bushes[i] = new THREE.Mesh(
     geometry = new THREE.DodecahedronGeometry(100, 1),
     material = new THREE.MeshBasicMaterial({ map: texture }),
   )
   scene.add(bushes[i])
 }
+bushes[0].position.set(800,-180,400)
+bushes[1].position.set(-700,-180,300)
 
-geometry = new THREE.CylinderGeometry(250, 250, 100, 64);
-material = new THREE.MeshBasicMaterial({ map: texture });
-const boshes = new THREE.Mesh(geometry,material);
-const boshes2 = new THREE.Mesh(geometry,material);
 
-bushes[0].position.set(300,-180,-200)
-bushes[1].position.set(300,-160,-100)
-bushes[2].position.set(300,-100,-150)
-bushes[3].position.set(250,-180,-250)
-bushes[3].scale.set(1.5,1,1)
-bushes[4].position.set(200,-180,-250)
-bushes[5].position.set(300,-50,-150);
-bushes[6].position.set(300,0,-200);
-bushes[6].scale.set(0.5,3,0.5)
-bushes[7].position.set(200,0,-250);
-bushes[7].scale.set(2,1,0.7);
-bushes[8].position.set(160,30,-250);
-bushes[8].scale.set(0.5,3,0.5);
-bushes[9].position.set(-150,40,-250);
-bushes[9].scale.set(0.2,2,0.5);
-bushes[10].position.set(-30,60,-250);
-bushes[10].scale.set(0.5,2,0.5);
-bushes[11].position.set(50,60,-250);
-bushes[11].scale.set(0.2,2,0.5);
-bushes[12].position.set(-250,50,-250);
-bushes[13].position.set(-10,-100,-250);
-bushes[13].scale.set(2,2,2);
-bushes[14].position.set(-300,50,50);
-bushes[14].scale.set(0.2,2,0.5);
-boshes.position.set(-250,-150,-150);
-boshes2.position.set(-250,-300,50);
 
-scene.add(boshes);
-scene.add(boshes2);
-
-boshes.rotation.z = Math.PI / 2;
-boshes2.rotation.z = Math.PI / 2;
 //Cloud
 
 //Planes
@@ -351,7 +339,7 @@ const planes = new THREE.Mesh(geometry,material);
 
 planes.rotation.x = Math.PI * 1.5
 
-planes.position.set(50,-200,0);
+planes.position.set(50,-229,0);
 
 scene.add(planes);
 
@@ -379,22 +367,14 @@ var materials = [
   
   
   const land1 = new THREE.Mesh(
-      new THREE.BoxGeometry(2000,1400,40),
+      new THREE.BoxGeometry(2000,1400,43),
       materials
   
   )
-  const land2 = new THREE.Mesh(
-    new THREE.BoxGeometry(1000,1600,40),
-    materials
-
-)
   land1.rotation.x = Math.PI * 0.5;
-  land2.rotation.x = Math.PI * 0.5;
 
-  land1.position.set(20,-250,0);
-  land2.position.set(-480,-250,100);
+  land1.position.set(20,-258,0);
   scene.add(land1);
-  scene.add(land2);
 
 
 //frame size
@@ -405,9 +385,15 @@ const sizes = {
 };
 
 
+hydro.position.y = -19
+const MinusCube = new THREE.Mesh(
+  new THREE.BoxGeometry(100,100,100),
+  new THREE.MeshBasicMaterial({color:"red"})
+)
+
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight,1,10000);
-camera.position.set(200, 200, 200);
+camera.position.set(1000, 2000 ,3000);
 camera.lookAt(wall.position);
 const pointLight = new THREE.PointLight("#fff", 1.5);
 scene.add(pointLight)
@@ -425,22 +411,36 @@ const renderer = new THREE.WebGLRenderer({
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
-controls.enableZoom = true;
-controls.enablePan = true;
 renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-scene.background = new THREE.Color(0x272928);
 renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
 
 
 // animate();
-
+let wind = "left"
 function controlAnimation() {
+  // if (wind === "left") {
+  //   clouds[0].position.x -= 0.3;
+  //   clouds[1].position.x -= 0.3;
+  //   clouds[2].position.z -= 0.3;
+  //   clouds[3].position.z -= 0.3;
+  //   if (clouds[0].position.x < -50) wind = "right";
+  // }
+
+  // if (wind == "right") {
+  //   clouds[0].position.x += 0.3;
+  //   clouds[1].position.x += 0.3;
+  //   clouds[2].position.z += 0.3;
+  //   clouds[3].position.z += 0.3;
+  //   if (clouds[0].position.x > 15) wind = "left";
+  // }
   requestAnimationFrame(controlAnimation);
   pointLight.position.x = camera.position.x
   pointLight.position.y = camera.position.y
   pointLight.position.z = camera.position.z
+  // controls.enableZoom = false;
+  // controls.enablePan = false;
 
   // required if controls.enableDamping or controls.autoRotate are set to true
   controls.update();
@@ -449,3 +449,8 @@ function controlAnimation() {
 }
 
 controlAnimation();
+const updateWindow = () => {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.render(scene, camera);
+}
+window.onresize = updateWindow
