e1ca64bff1dede3d8bebdfeb8ebeea26d42a1ed4
aldaraa <aldaraa@LEAP-15s-iMac.local>
2022-05-03 17:39:50 +0800

    05.03

--- common/scroll-animation.js
+++ common/scroll-animation.js
@@ -1,11 +1,17 @@
 const elements = document.querySelectorAll(".scroll-animated");
 
-window.addEventListener("scroll", () => {
+const handleScroll = () => {
   const scroll = window.scrollY + window.innerHeight;
-  Array.from(elements).forEach((element) => {
+  console.log(scroll, "scrol");
+  Array.from(elements).forEach((element, index) => {
     const top = element.offsetTop + element.getBoundingClientRect().height;
+    console.log(index, top);
     if (scroll > top) {
       element.classList.add("scroll-animation");
     }
   });
-});
+};
+
+window.addEventListener("scroll", handleScroll);
+
+handleScroll();
