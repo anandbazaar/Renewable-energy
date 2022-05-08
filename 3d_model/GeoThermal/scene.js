3a7b9bf806c47d79dc7523b6c8db1a609d4970fb
anandbazaar <temonation123@yahoo.com>
2022-05-07 19:57:30 -0700

    asd

--- 3d_model/GeoThermal/scene.js
+++ 3d_model/GeoThermal/scene.js
@@ -28,19 +28,20 @@ pointLight[3].position.set(0, 35, 335);
 pointLight[3].intensity = 1.5;
 pointLight[4].position.set(0, -120, 0);
 pointLight[4].intensity = 1.5;
-pointLight[5].position.set(0, 120, 0);
-pointLight[5].intensity = 0.2;
+pointLight[5].position.set(0, 150, -70);
+pointLight[5].intensity = 0.3;
 // pointLight[5].position.set(10, 150, 0);
 // pointLight[5].intensity = 0.1
 //objects
 
 all.position.set(0, 0, 0);
 all.add(land);
-all.add(smoke[0])
+
 all.add(tower);
 all.add(machine);
 all.add(standdecor)
 all.add(holders)
+all.add(factory);
 scene.add(all);
 
 const axesHelper = new THREE.AxesHelper( 100 );
@@ -57,12 +58,7 @@ renderer.render(scene, camera);
 const controls = new OrbitControls(camera, renderer.domElement);
 let ran = Math.random()
 function animate() {
-  smoke[0].position.y += 1
-  smoke[0].position.x += ran * -1
-  smoke[0].position.z += ran * -1
-  smoke[0].scale.x += ran
-  smoke[0].scale.z += ran
-  smoke[0].scale.y += ran
+
   requestAnimationFrame(animate);
   // controls.enableZoom = false;
   // controls.enablePan = false;
---
+++ 3d_model/GeoThermal/smoker.js
@@ -0,0 +1,120 @@
+//smoker
+const factory = new THREE.Group();
+const FacChimney  = new THREE.Group();
+
+const base = new THREE.Mesh(
+    new THREE.CylinderGeometry(
+        15,15,45,128
+    ),
+    new THREE.MeshStandardMaterial({color:0x808080})
+)
+const base2 = new THREE.Mesh(
+    new THREE.CylinderGeometry(
+        7,15,10,128
+    ),
+    new THREE.MeshStandardMaterial({color:0x808080})
+)
+const base3 = new THREE.Mesh(
+    new THREE.CylinderGeometry(
+        7,7,60,128
+    ),
+    new THREE.MeshStandardMaterial({color:0x808080})
+)
+const base4 = new THREE.Mesh(
+    new THREE.CylinderGeometry(
+        8,8,10,128
+    ),
+    new THREE.MeshStandardMaterial({color:0x808080})
+)
+FacChimney.add(base2)
+FacChimney.add(base)
+FacChimney.add(base3)
+FacChimney.add(base4)
+base3.position.set(0,50,0)
+base2.position.set(0,27.5,0)
+base4.position.set(0,80,0)
+FacChimney2 = FacChimney.clone()
+factory.add(FacChimney)
+FacChimney2.position.set(0,0,-60)
+factory.add(FacChimney2)
+factory.position.set(0,21,-50)
+//main body
+material1 = [
+    new THREE.MeshStandardMaterial({
+        color:0x808080,
+        roughness:0.1
+    }),
+    new THREE.MeshStandardMaterial({
+        color:0x808080,
+        roughness:0.1
+    }),
+    new THREE.MeshBasicMaterial({
+        color:0x404040,
+    }),
+    new THREE.MeshStandardMaterial({
+        color:0x404040,
+        roughness:0.1
+    }),
+    new THREE.MeshStandardMaterial({
+        color:0x404040,
+        roughness:0.1
+    }),
+    new THREE.MeshStandardMaterial({
+        color:0x404040,
+        roughness:0.1
+    }),
+]
+const MainBody = new THREE.Mesh(
+   new THREE.BoxGeometry(50,50,100),
+    material1
+    )
+MainBody.position.set(-20,0,-30)
+factory.add(MainBody)
+const HoleWay = new THREE.Mesh(
+    new THREE.BoxGeometry(20,10,20),
+    new THREE.MeshStandardMaterial({color:0x808080})
+)
+factory.add(HoleWay)
+HoleWay.position.set(-20,-25,-100)
+const MainTattoo = new THREE.Mesh(
+    new THREE.PlaneGeometry(95,40),
+    new THREE.MeshBasicMaterial({color:0x808080,side:THREE.DoubleSide})
+)
+MainTattoo.position.set(6,0,-30)
+MainTattoo2 = MainTattoo.clone()
+MainTattoo2.position.set(-46,0,-30)
+MainTattoo.rotation.y = Math.PI * 0.5
+factory.add(MainTattoo2)
+MainTattoo2.rotation.y = Math.PI * 1.5
+factory.add(MainTattoo)
+//pipes
+const piipe = new THREE.Mesh(
+    new THREE.BoxGeometry(
+        20,2,2
+    ),
+    new THREE.MeshStandardMaterial({
+        color:0x808080,
+        roughness:0.1
+    })
+)
+piipe.position.set(-15,40,0)
+const piipe2 = piipe.clone()
+piipe2.rotation.z = Math.PI * 0.5
+piipe2.position.set(-24,30,0)
+const piipe3 =piipe.clone()
+piipe3.position.set(-15,40,-60)
+const piipe4 = piipe.clone()
+piipe4.rotation.z = Math.PI * 0.5
+piipe4.position.set(-24,30,-60)
+const piipe5 = piipe.clone()
+piipe5.rotation.y = Math.PI * 0.5
+piipe5.position.set(-24,-10,-85)
+const piipe6 = piipe.clone()
+piipe6.rotation.z = Math.PI * 0.5
+piipe6.position.set(-24,-20,-94)
+factory.add(piipe)
+factory.add(piipe2)
+factory.add(piipe3)
+factory.add(piipe4)
+factory.add(piipe5)
+factory.add(piipe6)
