8f3dca9a5848a286debf42493e4ae275cff6372c
Biligt-Ochir <biligtburnee28@gmail.com>
2022-05-10 17:42:08 +0800

    almost done

--- quiz/app.js
+++ quiz/app.js
@@ -1,3 +1,5 @@
+
+// Initialize Cloud Firestore and get a reference to the service
 const questionNumber = document.querySelector(".question-number");
 const questionText = document.querySelector(".question-text");
 const optionContainer = document.querySelector(".option-container");
@@ -116,11 +118,18 @@ function next() {
 //    score:"total-correct";
 //    });
 // } else {
-//   // No user is signed in.
+//    No user is signed in.
+
 // }
+const uuid = window.localStorage.getItem("uuid");
+
+
+
 function quizOver() {
   quizBox.classList.add("hide");
   resultBox.classList.remove("hide");
+  const ref = db.collection('users').doc(`${uuid}`)
+ref.set({correctAnswers})
   quizResult();
 }
 
