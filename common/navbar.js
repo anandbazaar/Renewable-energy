7dd74ad5d128fef109dced32bc25ffadd71b68a0
maralmaa ulambayar <marlaa@LEAP-8s-iMac.local>
2022-05-14 18:59:39 +0800

    update

--- common/navbar.js
+++ common/navbar.js
@@ -1,13 +1,39 @@
-var lastScrollTop;
-var navbar = document.getElementById("navbar");
+const body = document.body;
+const nav = document.querySelector(".page-header nav");
+const menu = document.querySelector(".page-header .menu");
+const lottiePlayer = document.querySelector("lottie-player");
+const scrollUp = "scroll-up";
+const scrollDown = "scroll-down";
+let lastScroll = 0;
 
-window.addEventListener("scroll", function () {
-  var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-  if (scrollTop > lastScrollTop) {
-    navbar.style.top = "-80px";
-  } else {
-    navbar.style.top = "0";
+window.addEventListener("scroll", () => {
+  const currentScroll = window.pageYOffset;
+  if (currentScroll <= 0) {
+    body.classList.remove(scrollUp);
+    return;
   }
-  lastScrollTop = scrollTop;
-});
 
+  if (currentScroll > lastScroll && !body.classList.contains(scrollDown)) {
+    // down
+    body.classList.remove(scrollUp);
+    body.classList.add(scrollDown);
+    lottiePlayer.play();
+  } else if (
+    currentScroll < lastScroll &&
+    body.classList.contains(scrollDown)
+  ) {
+    // up
+    body.classList.remove(scrollDown);
+    body.classList.add(scrollUp);
+    lottiePlayer.stop();
+  }
+  lastScroll = currentScroll;
+});
+triggerMenu.addEventListener("click", () => {
+  body.classList.toggle("menu-open");
+});
+lottieWrapper.addEventListener("click", (e) => {
+  e.preventDefault();
+  triggerMenu.click();
+  body.classList.toggle("menu-open-with-lottie");
+});
