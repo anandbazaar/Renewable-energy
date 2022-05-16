96d3cdcde0264f278badfecda0dcf94e593bcb80
Maralmaa <maralmaa@Zorigs-MacBook-Pro.local>
2022-05-16 21:52:32 +0800

    update

--- common/navbar.js
+++ common/navbar.js
@@ -1,7 +1,6 @@
 const body = document.body;
 const nav = document.querySelector(".page-header nav");
 const menu1 = document.querySelector(".page-header .menu");
-const lottiePlayer = document.querySelector("lottie-player");
 const scrollUp = "scroll-up";
 const scrollDown = "scroll-down";
 let lastScroll = 0;
@@ -17,7 +16,6 @@ window.addEventListener("scroll", () => {
     // down
     body.classList.remove(scrollUp);
     body.classList.add(scrollDown);
-    lottiePlayer.play();
   } else if (
     currentScroll < lastScroll &&
     body.classList.contains(scrollDown)
@@ -25,15 +23,6 @@ window.addEventListener("scroll", () => {
     // up
     body.classList.remove(scrollDown);
     body.classList.add(scrollUp);
-    lottiePlayer.stop();
   }
   lastScroll = currentScroll;
-});
-triggerMenu.addEventListener("click", () => {
-  body.classList.toggle("menu-open");
-});
-lottieWrapper.addEventListener("click", (e) => {
-  e.preventDefault();
-  triggerMenu.click();
-  body.classList.toggle("menu-open-with-lottie");
 });
